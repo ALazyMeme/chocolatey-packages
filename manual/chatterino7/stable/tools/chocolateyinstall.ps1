c997663e983ac60805eecd11ce215e8484430877
ALazyMeme <jkeasley@icloud.com>
2021-08-06 22:36:24 +1000

    Revert "Update files to zip"
    This reverts commit af61f10c184b4cd456e261e0696531c37cebbe96.

--- manual/chatterino7/stable/tools/chocolateyinstall.ps1
+++ manual/chatterino7/stable/tools/chocolateyinstall.ps1
@@ -1,7 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'chatterino7-stable.zip'
+$fileLocation = Join-Path $toolsDir 'chatterino7-installer.exe'
 $packagename  = 'Chatterino7'
 
-Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino7-stable.zip" -Destination "C:\Program Files\Chatterino7" -PackageName "Chatterino7"
+$packageArgs = @{
+  packageName   = $packagename
+  fileType      = 'EXE'
+  softwareName  = 'Chatterino7*'
+  file			= $fileLocation
+  validExitCodes= @(0)
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /MERGETASKS="!vcredist" /DIR="C:\Program Files\Chatterino7"'
+  checksum      = 'A0D1C47936BE460CC8E830BE8B29CE1BC54B700EE76976E35BE72A877F2F68D5'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
 Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino7.lnk" -TargetPath "C:\Program Files\Chatterino7\chatterino.exe"
