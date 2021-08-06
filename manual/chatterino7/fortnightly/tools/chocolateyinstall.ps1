966744f4ddd7776546df5222f7610ffeb79e6108
ALazyMeme <jkeasley@icloud.com>
2021-08-06 22:16:12 +1000

    Revert "Update file paths"
    This reverts commit 1a0f1bef3002526bffd1193a776f958afe93813d.

--- manual/chatterino7/fortnightly/tools/chocolateyinstall.ps1
+++ manual/chatterino7/fortnightly/tools/chocolateyinstall.ps1
@@ -3,5 +3,5 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'chatterino7-fortnightly.zip'
 $packagename  = 'Chatterino7'
 
-Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino7-fortnightly.zip" -Destination "C:\Program Files (x86)\Chatterino7" -PackageName "Chatterino7"
-Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino7.lnk" -TargetPath "C:\Program Files (x86)\Chatterino7\chatterino.exe"
+Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino7-fortnightly.zip" -Destination "C:\Program Files\Chatterino7" -PackageName "Chatterino7"
+Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino7.lnk" -TargetPath "C:\Program Files\Chatterino7\chatterino.exe"
--- manual/chatterino7/fortnightly/tools/chocolateyuninstall.ps1
+++ manual/chatterino7/fortnightly/tools/chocolateyuninstall.ps1
@@ -1,2 +1,2 @@
-﻿Remove-Item -Path "C:\Program files (x86)\Chatterino7" -Force -Recurse
+﻿Remove-Item -Path "C:\Program files\Chatterino7" -Force -Recurse
 Remove-Item -Path "$env:USERPROFILE\Desktop\Chatterino7.lnk" -Force
