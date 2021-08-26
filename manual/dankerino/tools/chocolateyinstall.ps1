2205cfdc57f2acb5631113faac048649871f435b
ALazyMeme <jkeasley@icloud.com>
2021-08-27 01:46:26 +1000

    Update to meet requirements

--- manual/dankerino/tools/chocolateyinstall.ps1
+++ manual/dankerino/tools/chocolateyinstall.ps1
@@ -3,5 +3,5 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'dankerino.zip'
 $packagename  = 'Dankerino'
 
-Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\dankerino.zip" -Destination "C:\Program Files\Dankerino" -PackageName $packagename
-Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Dankerino.lnk" -TargetPath "C:\Program Files\Dankerino\chatterino.exe"
+Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\dankerino.zip" -Destination $toolsDir -PackageName $packagename
+Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Dankerino.lnk" -TargetPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\Dankerino\chatterino.exe"
--- manual/dankerino/tools/chocolateyuninstall.ps1
+++ manual/dankerino/tools/chocolateyuninstall.ps1
@@ -1,2 +1,2 @@
-﻿Remove-Item -Path "C:\Program files\Dankerino" -Force -Recurse
+﻿Remove-Item -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\Dankerino" -Force -Recurse
 Remove-Item -Path "$env:USERPROFILE\Desktop\Dankerino.lnk" -Force
