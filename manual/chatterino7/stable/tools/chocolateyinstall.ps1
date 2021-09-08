66096a46b0b72dba140318b1f22d7c1e2292989f
ALazyMeme <jkeasley@icloud.com>
2021-09-08 21:46:39 +1000

    Update again

--- manual/chatterino7/stable/tools/chocolateyinstall.ps1
+++ manual/chatterino7/stable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $fileLocation = Join-Path $toolsDir 'chatterino7-stable.zip'
 $packagename  = 'Chatterino7'
 
-Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino7-stable.zip" -Destination "$env:LOCALAPPDATA\Programs\Chatterino7" -PackageName "Chatterino7"
-Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino7.lnk" -TargetPath "$env:LOCALAPPDATA\Programs\Chatterino7\chatterino.exe"
+Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino7-stable.zip" -Destination $toolsDir
+Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino7.lnk" -TargetPath $toolsDir\chatterino.exe
