e809be0e86f9490b5aace76a4d489a1dbc320ddf
ALazyMeme <jkeasley@icloud.com>
2021-09-22 22:32:09 +1000

    update

--- manual/chatterino7/stable/tools/chocolateyuninstall.ps1
+++ manual/chatterino7/stable/tools/chocolateyuninstall.ps1
@@ -1,2 +1,1 @@
-﻿Remove-Item -Path "$env:LOCALAPPDATA\Programs\Chatterino7" -Force -Recurse
-Remove-Item -Path "$env:USERPROFILE\Desktop\Chatterino7.lnk" -Force
+﻿Remove-Item -Path "$env:USERPROFILE\Desktop\Chatterino7.lnk" -Force
