14b4462c0921cb257049241cf42373caa8af0889
ALazyMeme <jkeasley@icloud.com>
2021-10-09 14:17:48 +1000

    Update alpha build

--- manual/chatterino7/fortnightly/tools/chocolateyinstall.ps1
+++ manual/chatterino7/fortnightly/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $fileLocation = Join-Path $toolsDir 'chatterino7-fortnightly.zip'
 $packagename  = 'Chatterino7'
 
-Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino7-fortnightly.zip" -Destination "C:\Program Files\Chatterino7" -PackageName "Chatterino7"
-Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino7.lnk" -TargetPath "C:\Program Files\Chatterino7\chatterino.exe"
+Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino7-fortnightly.zip" -Destination $toolsDir
+Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino7.lnk" -TargetPath $toolsDir\chatterino.exe
--- manual/chatterino7/fortnightly/tools/chocolateyuninstall.ps1
+++ manual/chatterino7/fortnightly/tools/chocolateyuninstall.ps1
@@ -1,2 +1,1 @@
-﻿Remove-Item -Path "C:\Program files\Chatterino7" -Force -Recurse
-Remove-Item -Path "$env:USERPROFILE\Desktop\Chatterino7.lnk" -Force
+﻿Remove-Item -Path "$env:USERPROFILE\Desktop\Chatterino7.lnk" -Force
